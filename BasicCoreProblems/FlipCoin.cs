a4543cbd841ed86be0fec34bcabf16ed366161a0
PrajaktaShewale <shewalep94@gmail.com>
2022-12-05 09:30:46 +0530

    [Prajakta]Add:Calculate percentage of flip coin

---
+++ BasicCoreProblems/FlipCoin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblems
+{
+    internal class FlipCoin
+    {
+        double HeadCount = 0,TailCount=0,PercentageOfHead=0,PercentageOfTail=0;
+        public void CheckHeadTail()
+        {
+            Console.WriteLine("Eanter no. of times you want to flip the coin");
+            double NumofCount =Convert.ToInt32(Console.ReadLine());
+            for(int i=0;i<NumofCount;i++)
+            {
+                Random ranobj = new Random();
+                double randomnumber = ranobj.NextDouble();
+               // Console.WriteLine(randomnumber);
+                if(randomnumber <0.5)
+                {
+                    Console.WriteLine("Tail");
+                    TailCount++;
+                }
+                else
+                {
+                    Console.WriteLine("Head");
+                    HeadCount++;
+                }
+                
+            }
+            Console.WriteLine("total Tail count " + TailCount);
+            Console.WriteLine("total Head count " + HeadCount);
+
+            Console.WriteLine("******Percentage of Head and Tail******");
+            PercentageOfHead = HeadCount / NumofCount * 100;
+            Console.WriteLine("Percentage of Head" + PercentageOfHead);
+            PercentageOfTail = TailCount / NumofCount * 100;
+            Console.WriteLine("Percentage of Tail" + PercentageOfTail);
+        }
+    }
+}
---
+++ BasicCoreProblems/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace BasicCoreProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("Welcome to Basic Core Programs");
+                Console.WriteLine("1. Flip Coin" + "\n" + "2.Exit");
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        FlipCoin flip = new FlipCoin();
+                        flip.CheckHeadTail();
+                        break;
+                        default:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
