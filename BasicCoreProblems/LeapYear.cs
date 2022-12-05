58dea2b8db6fca44f1ab7c757514060a2674e652
PrajaktaShewale <shewalep94@gmail.com>
2022-12-05 10:06:10 +0530

    [Prajakta]Add:Find the Leap Year problem solution

---
+++ BasicCoreProblems/LeapYear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblems
+{
+    internal class LeapYear
+    {
+        public void FindLeap()
+        {
+            Console.WriteLine("Enter Year");
+            int Year = Convert.ToInt32(Console.ReadLine());
+            if (Year%4 == 0)
+            {
+                Console.WriteLine("Leap year");
+            }
+            else
+            {
+                Console.WriteLine("It is not a Leap Year");
+            }
+        }
+    }
+}
--- BasicCoreProblems/Program.cs
+++ BasicCoreProblems/Program.cs
@@ -6,16 +6,20 @@ namespace BasicCoreProblems
         static void Main(string[] args)
         {
             bool flag = true;
-            while(flag)
+            while(flag)//for reapeat input
             {
                 Console.WriteLine("Welcome to Basic Core Programs");
-                Console.WriteLine("1. Flip Coin" + "\n" + "2.Exit");
+                Console.WriteLine("1. Flip Coin" + "\n" + "2.Leap Year" + "\n" + "3.Exit");
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch (check)
                 {
                     case 1:
                         FlipCoin flip = new FlipCoin();
                         flip.CheckHeadTail();
+                        break;
+                        case 2:
+                            LeapYear leap = new LeapYear();
+                        leap.FindLeap();
                         break;
                         default:
                         flag = false;
