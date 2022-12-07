8d7e10cd5fc1be2ece0df625d6498ee0f969abe0
PrajaktaShewale <shewalep94@gmail.com>
2022-12-07 16:37:10 +0530

    [Prajakta]Add:Find alphabet is Vowel or Consonant problem solution

---
+++ BasicCoreProblems/VowelConsonant.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblems
+{
+    internal class VowelConsonant
+    {
+        public void Alphabet()
+        {
+            Console.WriteLine("Find given char is Vowel Or Consonant");
+            Console.WriteLine("Enter char to find");
+            char ch = Convert.ToChar(Console.ReadLine());
+            
+                switch (ch)
+                {
+                    case 'a':
+                        Console.WriteLine(ch + " is vowel");
+                        break;
+                    case 'i':
+                        Console.WriteLine(ch + " is vowel");
+                        break;
+                    case 'o':
+                        Console.WriteLine(ch + " is vowel");
+                        break;
+                    case 'u':
+                        Console.WriteLine(ch + " is vowel");
+                        break;
+                    case 'e':
+                        Console.WriteLine(ch +" is vowel");
+                        break;
+                    default:
+                        Console.WriteLine(ch + " is Consonant");
+                        break;
+                }
+        }
+    } 
+}
